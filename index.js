74f142e053bdbed4690846a86ca7fbccd41df583
Jai <jaijai.stell@gmail.com>
2023-03-20 15:25:27 -0700

    completes lab with comments

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// jais code
+let num1 = 2;
+let num2 = 31;
+//62 divided by 2 is 31 yay
+let num3 = 16;
+let num4 = 6;
+//I need an equation that will multiply to 62
+let multiply = num1 * num2;
+let mod = num3 % num4;
+//integer division which should be greater than 0
 
-// Write your code here
+//calculates a remainder equal to 4 (looks totally differnt in python)
+let max = Math.max(2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20);
+//returns 20 as the highest number in the set
+
+let random = Math.floor(Math.random() * 10) + 1;
+Math.isInteger(random);
